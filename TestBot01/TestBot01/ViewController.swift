34823145abbc818c8003b6182fe57bccbedf958f
Lee Hongik <ho-lee@gmo-click.com>
2017-06-12 12:18:32 +0900

    first push

---
+++ TestBot01/TestBot01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestBot01
+//
+//  Created by ho-lee on 2017/06/12.
+//  Copyright © 2017年 GMO Click HD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
